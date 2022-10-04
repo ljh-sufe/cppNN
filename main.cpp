a1ab0459f014877a3aeb29e3497c304c8e0c1a61
Junhao(Jonathan) Lan <88733865+ljh-sufe@users.noreply.github.com>
2022-10-04 05:18:47 -0500

    1

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#define INNODE 2 // number of inputs(in_features)
+#define HIDDENNODE 4 // 
+#define OUTNODE 1
+
+
+int main(){
+
+    using namespace std;
+    cout << "hello world" <<endl;
+    return 0;
+}
